347de538986085b13388a104a2fcf821908a4481
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-05-28 04:53:59 -0300

    fazendo os web components

--- js/api/api.js
+++ js/api/api.js
@@ -8,9 +8,60 @@ export const getAllCharacters = async () => {
 
     return data;
 }
+//personagem pelo id do personagem
+export const getCharacter = async (idCharacter) => {
+
+    const url = `https://rickandmortyapi.com/api/character/${idCharacter}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+//personagem pelo nome
+export const getCharacterByName = async (nameCharacter) => {
+
+    const url = `https://rickandmortyapi.com/api/character/?name=${nameCharacter}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+//personagem pelo status
+export const getCharacterByStatus = async (statusCharacter) => {
+
+    const url = `https://rickandmortyapi.com/api/character/?status=${statusCharacter}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+//personagem pela especie
+export const getCharacterBySpecies = async (speciesCharacter) => {
+
+    const url = `https://rickandmortyapi.com/api/character/?species=${speciesCharacter}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+//personagem pelo genero
+export const getCharacterByGender = async (genderCharacter) => {
+
+    const url = `https://rickandmortyapi.com/api/character/?species=${genderCharacter}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
 
 
 //PLANETAS
+//Retorna todos os planetas
 export const getPlanetas = async () => {
 
     const url = 'https://rickandmortyapi.com/api/location';
@@ -20,7 +71,18 @@ export const getPlanetas = async () => {
     console.log(data);
     return data;
 }
+//Retorna todos os planetas
+export const getPlanetaById = async (idPlaneta) => {
 
+    const url = `https://rickandmortyapi.com/api/location/${idPlaneta}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+//Retorna os residentes do planeta especificado
 export const getPlanetsResidents = async (idPlanet) => {
     const planeta = await getPlanetas()
     const jsonPlaneta = []
@@ -38,4 +100,25 @@ export const getPlanetsResidents = async (idPlanet) => {
     
 }
 
+
 //EPISÓDIOS
+//Retorna todos os episodios
+export const getEpisodes = async () => {
+
+    const url = `https://rickandmortyapi.com/api/episode`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+//Retorna todos o episodio pelo id
+export const getEpisodeById = async (idEpisode) => {
+
+    const url = `https://rickandmortyapi.com/api/episode/${idEpisode}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
--- js/web-components/personagens.js
+++ js/web-components/personagens.js
@@ -1,21 +1,15 @@
 'use strict'
 
-import { getAllGames, getPlataformas } from '../api/api.js'
-
-const allGames = await getAllGames();
-const get = await getPlataformas();
-
 class card extends HTMLElement {
     constructor() {
         super()
         this.shadow = this.attachShadow({ mode: 'open' });
         this.nome = get.title;
         this.image = null;
-        this.descricao = '#ff0000'
-        this.button = 'Read More'
+        this.descricao = null
     }
     static get observedAttributes(){
-        return['nome', 'foto', 'cor', 'turma']
+        return['nome', 'foto', 'descricao']
     }
 
     attributeChangedCallback(nameAttr, oldValue, newValue){
@@ -32,93 +26,58 @@ class card extends HTMLElement {
         const css = document.createElement('style');
         css.textContent = `
         .card {
-            width: 190px;
-            height: 254px;
-            border-radius: 1em;
-            padding: 1.9rem;
-            background: #f5f5f5;
-            position: relative;
+            width: 100%;
+            max-width: 300px;
+            height: 70px;
+            background: #353535;
+            border-radius: 20px;
             display: flex;
-            align-items: flex-end;
-            transition: 0.4s ease-out;
-            box-shadow: 0px 7px 20px rgba(43, 8, 37, 0.2);
-           }
-           
-           .card:before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            display: block;
-            width: 100%;
-            height: 100%;
-            background: rgba(238, 116, 116, 0.4);
-            z-index: 2;
-            transition: 0.5s;
-           }
-           
-           .card-info {
-            position: relative;
-            z-index: 3;
-            color: #f5f5f5;
-            opacity: 0;
-            transform: translateY(20%);
-            transition: 0.5s;
-           }
-           
-           /*Text*/
-           .text-title {
-            font-size: 1.5rem;
-            font-weight: 500;
-           }
-           
-           .text-body {
-            letter-spacing: 1px;
-            font-size: 0.9rem;
-            margin: 5px 0 15px 0;
-           }
-           
-           /*Button*/
-           .card-button {
-            padding: 0.6rem;
-            outline: none;
-            border: none;
-            border-radius: 4px;
-            background: #ee9ca7;
+            align-items: center;
+            justify-content: left;
+            backdrop-filter: blur(10px);
+            transition: 0.5s ease-in-out;
+        }
+        
+        .card:hover {
+            cursor: pointer;
+            transform: scale(1.05);
+        }
+        
+        .foto-personagem {
+            width: 50px;
+            height: 50px;
+            margin-left: 10px;
+            border-radius: 10px;
+            background: linear-gradient(#d7cfcf, #9198e5);
+        }
+        
+        
+        .textBox {
+            width: calc(100% - 90px);
+            margin-left: 10px;
             color: white;
+            font-family: 'Poppins' sans-serif;
+        }
+        
+        .textContent {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        
+        .span {
+            font-size: 10px;
+        }
+        
+        .nome-personagem {
+            font-size: 16px;
             font-weight: bold;
-            transition: 0.4s ease;
-           }
-           
-           /*Image*/
-           .card-img {
-            width: 100%;
-            height: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-            background: #ee9ca7;
-            background: linear-gradient(to bottom, #e7aeae, #ee9ca7);
-           }
-           
-           /*Hover*/
-           .card:hover {
-            transform: translateY(5%);
-           }
-           
-           .card:hover:before {
-            opacity: 1;
-           }
-           
-           .card:hover .card-info {
-            opacity: 1;
-            transform: translateY(0);
-           }
-           
-           .card-button:hover {
-            background: rgba(218, 77, 77, 0.4);
-            color: #f5f5f5;
-           }
+        }
+        
+        .especie-personagem {
+            font-size: 12px;
+            font-weight: lighter;
+        }
           
         
         `
@@ -129,37 +88,47 @@ class card extends HTMLElement {
         const card = document.createElement('div');
         card.classList.add('card');
 
-        const cardInfo = document.createElement('div');
-        cardInfo.classList.add('card-info');
-        cardInfo.textContent = this.nome;
+        const characterImage = document.createElement('img');
+        characterImage.classList.add('foto-personagem');
+        characterImage.textContent = this.image;
 
-        const momeCard = document.createElement('p');
-        momeCard.classList.add('text-title');
-        momeCard.textContent = this.turma;
+        const textBox = document.createElement('div');
+        textBox.classList.add('textBox');
 
-        const descricaoCard = document.createElement('p');
-        descricaoCard.classList.add('text-body');
-        descricaoCard.textContent = this.turma;
+        const textContent = document.createElement('div');
+        textContent.classList.add('textContent');
 
-        const button = document.createElement('button');
-        button.classList.add('card-button');
-        button.textContent = this.turma;
+        const nomePersonagem = document.createElement('p');
+        nomePersonagem.classList.add('nome-personagem');
+        nomePersonagem.textContent = this.nome;
 
-        card.append(nomeAluno, imagem, turma)
+        const descricaoPersonagem = document.createElement('p');
+        descricaoPersonagem.classList.add('especie-personagem');
+        descricaoPersonagem.textContent = this.turma;
+
+        textContent.append(nomePersonagem);
+        textBox.append(textContent, descricaoPersonagem);
+        card.append(characterImage, textBox);
 
         return card;
     }
 
-/* <div class="card">
-        <div class="card-img"></div>
-        <div class="card-info">
-            <p class="text-title">Card title</p>
-            <p class="text-body">Lorem Ipsum dolor sit amet</p>
-            <button class="card-button">Read More</button>
+/* 
+        <div class="card">
+            <img src="https://rickandmortyapi.com/api/character/avatar/253.jpeg" class="img" alt="">
+            <div class="textBox">
+                <div class="textContent">
+                    <p class="nome-personagem">Clans of Clash</p>
+                </div>
+                <p class="especie-personagem">Xhattmahs is not attacking your base!</p>
+                
+            </div>
         </div>
-    </div> */
+
+
+*/
 
 
 }
 
-customElements.define('card-dan', card)
+customElements.define('cards', card)
